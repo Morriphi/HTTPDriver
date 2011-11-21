daaac4b16cd9439e392e5c3c46ceadac627ca6df
Philip Morris <philiplmorris@googlemail.com>
2011-11-21 13:24:17 +0000

    Added tests to build script

--- build/build.ps1
+++ build/build.ps1
@@ -1,5 +1,17 @@
-Task Default -depends Build
+properties {
+	$base_dir = Resolve-Path .\..
+	$test_assemblies = (Get-ChildItem "$base_dir" -Recurse -Include *Test.dll -Name | Select-String "bin")
+}
+
+Task Default -depends Test
 
 Task Build {
-   Exec { msbuild "..\HttpDriver.sln" }
+   Exec { msbuild $base_dir\HttpDriver.sln }
+}
+
+Task Test -depends Build {
+	foreach($test_asm_name in $test_assemblies) {
+		Write-Host "Running Tests: " + $test_asm_name -for DarkGreen
+		Exec { ..\lib\NUnit.2.5.10.11092\tools\nunit-console.exe /nodots $base_dir\$test_asm_name }
+    }
 }
