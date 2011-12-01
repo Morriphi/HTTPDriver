93210f0ca664049ff1c5d6faa24524abc2c9c28c
Philip Morris <philiplmorris@googlemail.com>
2011-12-01 11:05:10 +0000

    Added clean to build script

--- build/build.ps1
+++ build/build.ps1
@@ -1,17 +1,26 @@
 properties {
 	$base_dir = Resolve-Path .\..
+	$build_artifacts = (Get-ChildItem "$base_dir" -Recurse -Include *.* -Name | Select-String "bin")
 	$test_assemblies = (Get-ChildItem "$base_dir" -Recurse -Include *Test.dll -Name | Select-String "bin")
 }
 
-Task Default -depends Test
+Task Default -depends Test 
 
-Task Build {
-   Exec { msbuild $base_dir\HttpDriver.sln }
+Task Clean {
+	Write-Host "Cleaning Up" -for DarkGreen
+	foreach($artifact in $build_artifacts) {
+		Remove-Item $base_dir\$artifact
+	}
+}
+
+Task Build -depends Clean {
+	Write-Host "Building" -for DarkGreen
+	Exec { msbuild $base_dir\HttpDriver.sln }
 }
 
 Task Test -depends Build {
 	foreach($test_asm_name in $test_assemblies) {
-		Write-Host "Running Tests: " + $test_asm_name -for DarkGreen
+		Write-Host "Running Test: " + $test_asm_name -for DarkGreen
 		Exec { ..\lib\NUnit.2.5.10.11092\tools\nunit-console.exe /nodots $base_dir\$test_asm_name }
     }
 }
